a4e17d628fe60dcef9003521787ec73e63782a20
Ottooooooo <naotoo7010.m@gmail.com>
2025-01-17 14:39:34 +0900

    [refactor]Solution.cppとTemplate.cppについて、ファイル名と実際の使われ方が合致していない状態を修正。

--- Template.cpp
+++ Template.cpp
@@ -1,19 +1,74 @@
-//C++20(GCC 12.2) こっち推奨
-/*
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
+
+    int N = 0, M = 0;
+    cin >> N >> M;
+
+    string S = "";
+    cin >> S;
+    
+    vector<int> A(N, 0);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    vector<vector<int>> B(N, vector<int>(N, 0));
+    int U;
+    int V;
+    for (int i = 0; i < M; i++)
+    {
+        cin >> U >> V;
+        U--;
+        V--;
+        way[U][V] = 1;
+        way[V][U] = 1;
+    }
+
+
+    A.emplace_back(5);
+
+    stack<int> st;
+    st.emplace(1); //st.push(1)
+    cout << st.top() << endl;
+    st.pop;
+    if (st.empty == true)
+    {
+        cout << "Now stack is empty" << endl;
+    }
+
+    queue<int> que1;
+    que1.emplace(1); //que.push(1)
+    cout << que1.front() << endl;
+    priority_queue<int> pque1; // <- デフォルトは降順ソートされる
+    priority_queue<int, vector<int>, greater<int>> pque2; // <- ここまで書くと昇順ソートにしてくれる
+    cout << pque1.top() << endl;
+
+    deque<int> deq;
+    deq.emplace_front(v); //deq.front(), deq.pop_front()
+    deq.emplace_back(v); //deq.back(), deq.pop_back()
+    cout << deq[i] << endl;
+
+    map<string, int> mp; // keyの型がstring, valueがint
+    mp["Hello"] = 5;
+
+    set<int> s;
+    s.insert(5); // setに5を格納, {5}
+    s.insert(9); // setに9を格納, {5, 9}
+    s.insert(-3); // setに-3を格納, {-3, 5, 9}
+    s.insert(9); // 9は既にあるので要素は変わらない, {-3, 5, 9}
+    auto it = s.find(5); // 5はset内にあるので，イテレータを返す
+    int it = *it; // v == 5,イテレータをintとして拾うには*を付ける
+    it = s.find(0); // 0はないので，it == s.end() となる
+    cout << s.count(-3); //-3はあるので、1
+    cout << s.count(7); //7はないので、0
+    it = s.lower_bound(3); //3以上で最小の値のイテレータ,今回3
+    it = s.upper_bound(3); //3超えで最小の値のイテレータ,今回5
+    it--; //イテレータは場所を示すのでインクリやデクリができる
+    cout << *it <<endl; //3
+
+    int ans = 0;
+    cout << ans <<endl;
+    
     return 0;
 }
-*/
-
-
-
-//C++(Clang 10.0.0)
-/*
-#include<iostream>
-using namespace std;
-int main() {
-    return 0;
-}
-*/
