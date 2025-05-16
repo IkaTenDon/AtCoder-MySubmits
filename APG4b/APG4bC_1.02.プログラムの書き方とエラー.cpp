8b62026f0d3a88c793ad9342cee59d5746194bb0
Ottooooooo <naotoo7010.m@gmail.com>
2025-05-16 19:52:01 +0900

    2025_05_16_SZPP

---
+++ APG4b/APG4bC_1.02.プログラムの書き方とエラー.cpp
@@ -0,0 +1,11 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//   cout << "a";
+//   cout << "b" << endl;
+//   cout << "c" << "d" << endl;
+// }
+
+#include <bits/stdc++.h>
+using namespace std;int main(){cout<<"a";cout<<"b"<<endl;cout<<"c"<<"d"<<endl;}
